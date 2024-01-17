c02c4f7b5c6b558de1a523ab50c5fe963a03f3ba
AbdurrahmanAKYEL <155827715+AbdurrahmanAKYEL@users.noreply.github.com>
2024-01-17 19:03:00 +0300

    Add files via upload

---
+++ Siber Vatan/sibervatan.py
@@ -0,0 +1,29 @@
+# # sayi1 = 42
+# # sayi2 = -17
+# # sayi3 = 1.8
+# # sayi4 = -3.5
+# # dogru_mu = True
+# # yanlis_mi = False
+# # ondalik1 = 3.14
+# # ondalik2 = 0.5
+
+# # sayi_float = float(sayi1)
+# # print("int to float", sayi_float)
+
+# # sayi_int = int(ondalik1)
+# # print("float to int",sayi_int)
+
+# # sayi_bool = int(dogru_mu)
+# # sayi_bool1 = int(yanlis_mi)
+# # print("bool to int", sayi_bool1)
+# # print(type(sayi_bool1))
+
+# # kullanici_ad = input("Lütfen Adinizi Giriniz")
+# # print("Hosgeldin",kullanici_ad)
+# sayi1 = int(input("İlk sayiyi girin"))
+# sayi2 = int(input("İkinci sayiyi girin"))
+# print(sayi1+sayi2)
+
+# sayi3 = input ("1. sayiyi girin")
+# sayi4 = input ("2. sayiyi girin")
+# print(int(sayi3)+int(sayi4))
