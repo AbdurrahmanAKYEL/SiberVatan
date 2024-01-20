794255640bfe3b4f2d0516123ef1fb54dd5dd5be
AbdurrahmanAKYEL <155827715+AbdurrahmanAKYEL@users.noreply.github.com>
2024-01-20 16:27:34 +0300

    Add files via upload

---
+++ Siber Vatan/sibervatan 3.py
@@ -0,0 +1,77 @@
+# liste = [1,2,3,4,5]
+# print(liste)
+# print(liste[4])
+# print(type(liste))
+
+# liste[4] = 50
+# print(liste)
+
+# alt_liste = liste[2:4]
+# print(alt_liste)
+
+# tuple
+# tuple = (10,20,30)
+# print(tuple)
+# print(tuple[1])
+
+#  tuple[1] = 50 
+#  print(tuple)
+# alt_tuple = tuple[1:4]
+# print(alt_tuple)
+
+# kümeler
+# kume = {100,200,300,400,500,100}
+# print(kume)
+# kume.add(700)
+# print(kume)
+# kume.update({400,900})
+# print(kume)
+
+# dict dictionery sözlük
+# dict = {"anahtar1" : "deger1" ,"anahtar2" : "deger2"}
+# print(dict)
+
+# deger = dict["anahtar1"]
+# print(deger)
+
+# liste = list(kume)
+# print(kume)
+# print(type(kume list))
+
+# list_keys = list(dict.keys()) 
+# print(type(list_keys))
+# print(list_keys)
+
+# list_values = list(dict.values())
+# print(type(list_values))
+# print(list_values)
+
+# sayilar = [10,8,5,3,15]
+# en_buyuk = max(sayilar)
+# en_kucuk = min(sayilar)
+
+# print(en_buyuk)
+# print(en_kucuk)
+
+# sayilar.append(20)
+# sayilar.append(1)
+
+# yeni_en_buyuk = max(sayilar)
+# yeni_en_kucuk = min(sayilar)
+# print(yeni_en_buyuk)
+# print(yeni_en_kucuk)
+# print(sayilar)
+
+# sayilar.sort()
+# print(sayilar)
+
+# sayilar.reverse()
+# print(sayilar)
+
+# print(sayilar.count(10))
+
+# aralik = range(1,100)
+# print(list(aralik))
+# import random
+# rasgele_sayi = random.randint(1,100)
+# print("tutulan sayi",rasgele_sayi)
