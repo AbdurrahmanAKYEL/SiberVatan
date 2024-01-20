b8c895336439802fab506d7ac8bb1f7e2cb3bb0d
AbdurrahmanAKYEL <155827715+AbdurrahmanAKYEL@users.noreply.github.com>
2024-01-20 13:53:27 +0300

    Add files via upload

---
+++ sibervatan 2.py
@@ -0,0 +1,41 @@
+# Ad = "Abdurrahman"
+# Soyad = "Akyel"
+# Yas = "16"
+# karsilama = "Benim Adim " + Ad + " " + Soyad + " " + " Yasim " + Yas + " Hoşgeldin "
+# print (karsilama) 
+# uzunluk = len(karsilama)
+# print(uzunluk)
+# print(karsilama[43]) 
+# print(uzunluk-1)
+# print(karsilama[-1])
+# print(karsilama[0:10])
+# print(karsilama[11:48])
+# print(karsilama[::-1])
+
+# karsilama_upper = karsilama.upper()
+# print(karsilama_upper)
+
+# # karsilama_lower = karsilama.lower()
+# print(karsilama_lower)
+
+# karsilama_strip = karsilama.strip()
+# print(karsilama_strip)
+
+# karsilama_split = karsilama.split()
+# print(karsilama_split)
+# print(karsilama_split[4])
+
+# karsilama_join = "-".join(karsilama)
+# print(karsilama_join)
+
+# karsilama_find  = karsilama.find("Abdurrahman")
+# print(karsilama_find)
+
+# karsilama_startswith = karsilama.startswith("A")
+# print(karsilama_startswith)
+
+# karsilama_endswith = karsilama.endswith("n")
+# print(karsilama_endswith)
+
+# karsilama_replace = karsilama.replace("Abdurrahman","Berat")
+# print(karsilama_replace)
