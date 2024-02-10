2aa37f499c29e35a24c76f77a6ffac1709f507e3
AbdurrahmanAKYEL <155827715+AbdurrahmanAKYEL@users.noreply.github.com>
2024-02-10 16:35:57 +0300

    Create SiberVatan 6.py

---
+++ Siber Vatan/SiberVatan 6.py
@@ -0,0 +1,36 @@
+# # meyveler = ["kivi", "elma" , "limon" , "bögurtlen"]
+# # print(meyveler)
+
+# # sayilar=[10,20,30,40,50]
+# # toplam=0
+# # for sayi in sayilar:
+# #     toplam += sayi
+
+# # print(toplam)
+
+# # meyveler = ["kivi", "elma" , "limon" , "bögurtlen"]
+# # for meyve in meyveler:
+# #     print(meyve)
+
+# # sayilar=[2,3,6,7,8,89,43,76,34,23]
+# # for sayi in sayilar:
+# #     if sayi % 2 == 1:
+# #         print(sayi)
+# #     else:
+# #         continue
+
+
+# # sayi= int(input("bir sayi girin"))
+
+# # sayaç = 0
+# # while sayaç<=sayi:
+# #     print(sayaç)
+# #     sayaç+=1
+# n=0
+# sayılistesi=[]
+# while n <=5:
+#     sayı=int(input("bir sayı girin:s"))
+#     sayılistesi.append(sayı)
+#     n+=1
+#     for i in sayılistesi:
+#         print(i)
